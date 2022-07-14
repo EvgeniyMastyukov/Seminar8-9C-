4ab6678a11e0718a28574d5db4ae5eb1c2d0bd80
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-07-14 14:37:49 +0300

    Add Task 56 Dom Seminar8

---
+++ Task56_Dom_Seminar8/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить
+// строку с наименьшей суммой элементов.
+Console.Write("Введите количество строк m: ");
+byte rows = Convert.ToByte(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+byte columns = Convert.ToByte(Console.ReadLine());
+
+int[,] CreateArray(int m, int n)
+{
+    Random random = new Random();
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = random.Next(10);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] myArray = CreateArray(rows, columns);
+System.Console.WriteLine();
+PrintArray(myArray);
+
+
+int[] row = new int[myArray.GetLength(0)];
+
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        sum += myArray[i, j];
+    }
+    row[i] = sum;
+}
+
+System.Console.WriteLine();
+PrintOneArray(row);
+Console.WriteLine($"Строка {FindIndexMin(row)} массива с наименьшей суммой элементов {row[FindIndexMin(row)]}");
+
+
+void PrintOneArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + "  ");
+    }
+    System.Console.WriteLine();
+}
+
+int FindIndexMin(int[] arr)
+{
+    int imin = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < arr[imin]) imin = i;
+    }
+    return imin;
+}
+
