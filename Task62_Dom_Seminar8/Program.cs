dad10b285527aa1cd3bb97bd3552020730016b47
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-07-19 16:49:57 +0300

    Add Task 62_Dom_V2

---
+++ Task62_Dom_Seminar8/Program.cs
@@ -0,0 +1,92 @@
+﻿//Задача 62: Заполните спирально массив 4 на 4.
+//1  2  3  4
+//12 13 14 5
+//11 16 15 6
+//10  9  8 7
+
+// Console.Write("Введите количество строк m: ");
+// byte rows = Convert.ToByte(Console.ReadLine());
+// Console.Write("Введите количество столбцов n: ");
+// byte columns = Convert.ToByte(Console.ReadLine());
+
+int[,] CreateArraay(int m, int n)
+{
+    Random random = new Random();
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = 0;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//int[,] myArray = CreateArraay(rows, columns);
+System.Console.WriteLine();
+//PrintArray(myArray);
+
+
+int[] arr1 = new int[16];
+int col = 1;
+for (int i = 0; i < arr1.Length; i++)
+{
+    arr1[i] = col;
+    col++;
+}
+
+void PrintOneArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + "\t");
+    }
+}
+
+System.Console.WriteLine();
+PrintOneArray(arr1);
+
+int[,] myArray = new int [,]
+{
+{1,1,1,1,1,1},
+{1,0,0,0,0,1},
+{1,0,0,0,0,1},
+{1,0,0,0,0,1},
+{1,0,0,0,0,1},
+{1,1,1,1,1,1},
+};
+
+void FillSpiral(int m, int n)
+{
+    //m = 0; n = 0;
+    int num = 1;
+
+    if (myArray[m, n] == 0)
+    {
+        myArray[m, n] = num;
+        num++;
+    }
+
+    FillSpiral(m, n + 1);
+    FillSpiral(m + 1, n);
+    FillSpiral(m, n - 1);
+    FillSpiral(m - 1, n);
+
+}
+FillSpiral(1, 1);
+System.Console.WriteLine();
+PrintArray(myArray);
+
---
+++ Task62_Dom_V2/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задача 62: Заполните спирально массив 4 на 4.
+//1  2  3  4
+//12 13 14 5
+//11 16 15 6
+//10  9  8 7
+
+int[,] array = new int[4, 4];
+int num = 1;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j <= array.GetLength(1); j++)
+    {
+        if (j < array.GetLength(1))
+        {
+            array[i, j] = num;
+            num++;
+        }
+        else
+        {
+            int k = 0;
+            int r = 0;
+
+            for (k = i + 1; k < array.GetLength(0); k++) //5,6,7
+            {
+                array[k, j - 1] = num;
+                num++;
+            }
+
+            for (r = j - 2; r >= 0; r--)   //8,9,10
+            {
+                array[k - 1, r] = num;
+                num++;
+            }
+
+            r = r + 1;
+            for (k = k - 2; k >= 1; k--) //11,12
+            {
+                array[k, r] = num;
+                num++;
+            }
+            k = k + 1;
+            for (r = r + 1; r < array.GetLength(1) - 1; r++)
+            {
+                array[k, r] = num;
+                num++;
+            }
+            System.Console.WriteLine($"k = {k}");
+            System.Console.WriteLine($" r = {r}");
+            k += 1;
+            for (r = r - 1; r > 0; r--)
+            {
+                array[k, r] = num;
+                num++;
+            }
+        }
+
+    }
+    break;
+}
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+System.Console.WriteLine();
+PrintArray(array);
