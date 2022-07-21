763258db94759556d2450acaa630f15979f7519d
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-07-21 14:12:06 +0300

    Add Task 66 Dom

---
+++ Task66_Dom_Seminar9/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке 
+//от M до N. Выполнить с помощью рекурсии. M = 1; N = 15 -> 120   M = 4; N = 8. -> 30
+
+Console.Write("Задайте  число M: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Задайте  число N: ");
+int numN = int.Parse(Console.ReadLine());
+
+int FindSumOfRange1(int m, int n) // m < n
+{
+    if(m == n) return m;
+    else return n + FindSumOfRange1(m,n-1);
+}
+
+int FindSumOfRange2(int m, int n) // m > n
+{
+    if(m == n) return m;
+    else return m + FindSumOfRange2(m-1,n);
+}
+if(numM <= numN) Console.WriteLine($"Summa = {FindSumOfRange1(numM, numN)}"); 
+if(numM > numN) Console.WriteLine($"Summa = {FindSumOfRange2(numM, numN)}"); 
+
+
