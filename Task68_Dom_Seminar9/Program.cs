eec2afa64559f6add900b4816bb156f8892778a2
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-07-23 00:33:36 +0300

    add task68, task 70

---
+++ Task68_Dom_Seminar9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Задайте  число m: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Задайте  число n: ");
+int numN = int.Parse(Console.ReadLine());
+
+int Akkerman(int m, int n )
+{
+    if(m == 0) return n + 1;
+    else if ((n == 0) && (m!=0)) return Akkerman(m-1,1);
+    else return Akkerman(m-1, Akkerman(m,n-1)); 
+
+}
+int res = Akkerman(numM, numN);
+System.Console.WriteLine($"Функция Аккермана = {res}");
---
+++ Task70_Seminar10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт первые N чисел,
+// для которых каждое следующее равно сумме двух предыдущих.
+// 3 и 4, N = 5 -> 3 4 7 11 18               6 и 10, N = 4 -> 6 10 16 26
+
+Console.WriteLine("Задайте первое число a: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте второе число b: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте длину последовательности N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+void FindOrder(int a, int b, int n)
+{
+    if (n == 0) return;
+    Console.Write($"{a} ");
+    int temp = b;
+    b = a + b;
+    a = temp;
+    FindOrder(a, b, n - 1);
+}
+FindOrder(numberA, numberB, numberN);
+
+
